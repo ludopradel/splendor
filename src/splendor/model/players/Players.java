ba54638e9cb12bf5b5df55a1c8f13b63d05d5ec9
ludopradel <ludovic.pradel@gmail.com>
2015-03-19 23:42:16 +0100

    player_could_be_added

--- src/splendor/model/players/Players.java
+++ src/splendor/model/players/Players.java
@@ -1,14 +1,23 @@
 package splendor.model.players;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 
 public class Players {
 
+	List<Player> players = new ArrayList<Player>();
 
 	public Players() {
 	}
 
 	public Integer numbers() {
-		return 0;
+		return players.size();
+	}
+
+	public void add(Player player) {
+		players.add(player);
 	}
 
 }
--- test/splendor/model/players/PlayersTest.java
+++ test/splendor/model/players/PlayersTest.java
@@ -8,8 +8,15 @@ import org.junit.Test;
 public class PlayersTest {
 	
 	@Test
-	public void player_list_is_initially_empty() throws Exception {
+	public void player_list_should_initially_be_empty() throws Exception {
 		Players players = new Players();
 		assertThat(players.numbers(), is(0));
+	}
+	
+	@Test
+	public void player_could_be_added() throws Exception {
+		Players players = new Players();
+		players.add(new Player("Pierre"));
+		assertThat(players.numbers(), is(1));
 	}
 }
