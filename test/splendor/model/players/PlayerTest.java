1875ebf263d65980a01df5f3ac240ef73541d567
ludopradel <ludovic.pradel@gmail.com>
2015-03-28 15:14:53 +0100

    refactoring: extract player into setup

--- test/splendor/model/players/PlayerTest.java
+++ test/splendor/model/players/PlayerTest.java
@@ -4,6 +4,7 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import splendor.exceptions.ColorPickedException;
@@ -15,18 +16,22 @@ import splendor.model.gems.PlayerGems;
 
 public class PlayerTest {
 
+	private Player a_player;
+	
+	@Before
+	public void setUp() {
+		a_player = new Player("Pierre");
+	}
 	
 	@Test
 	public void a_player_should_be_created_with_a_name() throws Exception {
-		Player player = new Player("Pierre");
-		assertThat(player.name(), is("Pierre"));
+		assertThat(a_player.name(), is("Pierre"));
 	}
 
 	@Test
 	public void when_player_picks_3_gems_then_stock_decrease() throws Exception {
-		Player player = new Player("Pierre");
 		GemStock gemStock = GemStock.initialise();
-		player.pickThreeDifferentGems(gemStock, Gem.RED, Gem.GREEN, Gem.BLUE);
+		a_player.pickThreeDifferentGems(gemStock, Gem.RED, Gem.GREEN, Gem.BLUE);
 		assertThat(gemStock.numberOf(Gem.RED), is(6));
 		assertThat(gemStock.numberOf(Gem.GREEN), is(6));
 		assertThat(gemStock.numberOf(Gem.BLUE), is(6));
@@ -34,55 +39,49 @@ public class PlayerTest {
 	
 	@Test
 	public void a_new_player_dont_have_any_gem_at_the_beginning() throws Exception {
-		Player player = new Player("Pierre");
-		PlayerGems playerGems = player.gemStock();
+		PlayerGems playerGems = a_player.gemStock();
 		assertThat(playerGems.isEmpty(), is(true));
 	}
 	
 	@Test
 	public void when_player_picks_gems_then_player_stock_is_increased() throws Exception {
-		Player player = new Player("Pierre");
 		GemStock gemStock = GemStock.initialise();
-		player.pickThreeDifferentGems(gemStock, Gem.RED, Gem.GREEN, Gem.BLUE);
-		PlayerGems playerGems = player.gemStock();
+		a_player.pickThreeDifferentGems(gemStock, Gem.RED, Gem.GREEN, Gem.BLUE);
+		PlayerGems playerGems = a_player.gemStock();
 		assertThat(playerGems.isEmpty(), is(false));
 	}
 	
 	@Test
 	public void when_player_picks_two_identical_gems_then_his_stock_contains_gems_picked() throws Exception {
-		Player player = new Player("Pierre");
 		GemStock gemStock = GemStock.initialise();
-		player.pickTwoIdenticalGems(gemStock, Gem.RED);
-		PlayerGems playerGems = player.gemStock();
+		a_player.pickTwoIdenticalGems(gemStock, Gem.RED);
+		PlayerGems playerGems = a_player.gemStock();
 		
 		assertThat(playerGems, equalTo(stockWithTwoRedGems()));
 	}
 
 	@Test
 	public void when_player_picks_gems_then_his_stock_contains_gems_picked() throws Exception {
-		Player player = new Player("Pierre");
 		GemStock gemStock = GemStock.initialise();
-		player.pickThreeDifferentGems(gemStock, Gem.RED, Gem.GREEN, Gem.BLUE);
-		PlayerGems playerGems = player.gemStock();
+		a_player.pickThreeDifferentGems(gemStock, Gem.RED, Gem.GREEN, Gem.BLUE);
+		PlayerGems playerGems = a_player.gemStock();
 		
 		assertThat(playerGems, equalTo(expectedPlayerGems()));
 	}
 	
 	@Test(expected=ColorPickedException.class)
 	public void on_a_three_gems_picks_player_could_not_pick_two_times_the_same_color() throws Exception {
-		Player player = new Player("Pierre");
 		GemStock gemStock = GemStock.initialise();
-		player.pickThreeDifferentGems(gemStock, Gem.RED, Gem.GREEN, Gem.RED);
+		a_player.pickThreeDifferentGems(gemStock, Gem.RED, Gem.GREEN, Gem.RED);
 	}
 
 	@Test
 	public void player_could_pick_a_card_in_his_hand() throws Exception {
-		Player player = new Player("Pierre");
 		LevelOneCards a_card = new LevelOneCards();
-		player.pickCard(a_card);
+		a_player.pickCard(a_card);
 		PlayerHand expectedHand = new PlayerHand();
 		expectedHand.add(a_card);
-		assertThat(player.cardsInHand(), is(expectedHand));
+		assertThat(a_player.cardsInHand(), is(expectedHand));
 	}
 	
 	
